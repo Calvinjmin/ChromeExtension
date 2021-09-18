c626bac7989761451f9e71578e7f712da3a19159
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2021-09-18 16:34:37 -0400

    Timer Extension: Am or PM

--- timer.js
+++ timer.js
@@ -4,5 +4,13 @@ document.getElementById("showTime").onclick = function() {showTime()};
 // Display Current Time
 function showTime() {
     let date = new Date();
-    document.getElementById("time").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    let amOrPm = "AM";
+    let hour = date.getHours();
+    if ( hour > 12 ) {
+        hour = (date.getHours() - 12)
+        amOrPm = "PM";
+    }
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    document.getElementById("time").innerHTML = hour + ":" + minutes + ":" + seconds + " " + amOrPm;
 }
