c19cacf3be747524b30697079e73815d2a22be73
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2021-09-21 15:56:41 -0400

    Show Time + Grab Selected Value

--- timer.js
+++ timer.js
@@ -1,7 +1,26 @@
 // Link On Click Listener
 document.getElementById("showTime").onclick = function() {showTime()};
+document.getElementById("startTimer").onclick = function() {startTimer()};
+
+// Constants
+const timeMap = new Map();
+timeMap.set("10m", 600);
+timeMap.set("20m", 1200);
+timeMap.set("30m", 1800);
+timeMap.set("1h", 3600);
 
 // Display Current Time
+function startTimer() {
+    //Select Element
+    const selectedTimeOpt = document.querySelector("#timer-value");
+    const selectedTime = [].filter
+        .call(selectedTimeOpt.options, option => option.selected)
+        .map(option => option.value);
+
+    //Selected Time is the Value Input
+    alert(timeMap.get(selectedTime[0]));
+}
+
 function showTime() {
     let date = new Date();
     let amOrPm = "AM";
